aa6701de30a99e54862aeb5622d573f6ab594987
Eric Baker <eabigm@rit.edu>
2025-01-21 10:46:25 -0500

    week 2 day 1 playing around

---
+++ 2D Project/Assets/Scripts/Day1demo.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Day1demo : MonoBehaviour
+{
+    public int favNum = 34;
+
+    public Transform followTarget;
+
+    public GameObject camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody testingCam = camera.GetComponent<Rigidbody>();
+
+        testingCam.mass = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(favNum);
+
+        //transform.position = followTarget.position;
+
+
+    }
+}
